7b3fe2d8a23b40f1455bc9928d163157334d3fe0
Hzye <81663786+Hzye@users.noreply.github.com>
2025-01-31 22:12:40 +1100

    first commit

---
+++ agents.py
@@ -0,0 +1,56 @@
+from crewai import Agent
+from textwrap import dedent
+from langchain.llms import OpenAI, Ollama
+from langchain_openai import ChatOpenAI
+
+
+"""
+team:
+- high school math subject coordinator
+- high school math teacher
+- math olympiad
+
+agents must be result driven + have a clear goal in mind
+role == job title
+goal should be actionable
+backstory == resume
+"""
+
+class CustomAgents:
+    def __init__(self):
+        self.OpenAIGPT35 = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
+        self.OpenAIGPT4 = ChatOpenAI(model_name="gpt-4", temperature=0.7)
+        self.Ollama = Ollama(model="openhermes")
+
+    def math_coordinator_agent(self):
+        return Agent(
+            role="Define agent 1 role here",
+            backstory=dedent(f"""Define agent 1 backstory here"""),
+            goal=dedent(f"""Define agent 1 goal here"""),
+            # tools=[tool_1, tool_2],
+            allow_delegation=False,
+            verbose=True,
+            llm=self.OpenAIGPT35,
+        )
+
+    def math_teacher_agent(self):
+        return Agent(
+            role="Define agent 2 role here",
+            backstory=dedent(f"""Define agent 2 backstory here"""),
+            goal=dedent(f"""Define agent 2 goal here"""),
+            # tools=[tool_1, tool_2],
+            allow_delegation=False,
+            verbose=True,
+            llm=self.OpenAIGPT35,
+        )
+
+    def math_olympiad_agent(self):
+        return Agent(
+            role="Define agent 2 role here",
+            backstory=dedent(f"""Define agent 2 backstory here"""),
+            goal=dedent(f"""Define agent 2 goal here"""),
+            # tools=[tool_1, tool_2],
+            allow_delegation=False,
+            verbose=True,
+            llm=self.OpenAIGPT35,
+        )
